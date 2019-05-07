9304826863bdc96432889ea2d362639d81dbe70d
cuijunyong <cuijunyong@qipeng.com>
2019-05-07 16:16:33 +0800

    去掉端口

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd love-service-impl
 ### nohup java -jar love-service.jar --server.port=18903 &
-nohup java -jar -Xms256m -Xmx256m -Xmn128m -XX:SurvivorRatio=6 -XX:MaxMetaspaceSize=64m -XX:MetaspaceSize=128m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:ConcGCThreads=1 -Dapp.name=love-service -Dapp.group=test -Dserver.tomcat.accesslog.directory=/home/admin/love-service/logs -XX:CMSInitiatingOccupancyFraction=70 love-service.jar --server.port=18903 &
+nohup java -jar -Xms128m -Xmx256m -XX:SurvivorRatio=6 -XX:MaxMetaspaceSize=64m -XX:MetaspaceSize=64m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:ConcGCThreads=1 -Dapp.name=love-service -Dapp.group=test -Dserver.tomcat.accesslog.directory=/home/admin/love-service/logs -XX:CMSInitiatingOccupancyFraction=70 love-service.jar --server.port=18903 &
